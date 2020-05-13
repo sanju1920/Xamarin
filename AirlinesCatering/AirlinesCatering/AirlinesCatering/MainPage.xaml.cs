204b6d47986ca167bf28f9454d97495daefb7001
sanjaykumarmahato <sanjaykumar.mahato@kelltontech.com>
2020-05-13 17:27:43 +0530

    Nativation Bar

--- AirlinesCatering/AirlinesCatering/AirlinesCatering/MainPage.xaml.cs
+++ AirlinesCatering/AirlinesCatering/AirlinesCatering/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AirlinesCatering.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,6 +17,18 @@ namespace AirlinesCatering
         public MainPage()
         {
             InitializeComponent();
+            masterPage.listView.ItemSelected += OnItemSelected;
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as NavigationItems;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.listView.SelectedItem = null;
+                IsPresented = false;
+            }
         }
     }
 }
